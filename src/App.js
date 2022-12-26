dd1a1cc42734cc18015ef976d42703dd4b316257
SvitlanaTymchii <109731357+SvitlanaTymchii@users.noreply.github.com>
2022-12-26 13:36:15 +0000

    Bootstrap is addded

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App
-      <footer>
-        This project is created by Svitlana Tymchii and is{" "}
-        <a
-          href="https://github.com/SvitlanaTymchii/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is created by Svitlana Tymchii and is{" "}
+          <a
+            href="https://github.com/SvitlanaTymchii/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
