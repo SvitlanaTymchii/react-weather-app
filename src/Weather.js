00f5309d7d8f404cbdf1146611b9dc758ecdfb3d
SvitlanaTymchii <109731357+SvitlanaTymchii@users.noreply.github.com>
2023-01-03 20:10:25 +0000

    CSS-align of temperature and units are added

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
+  let [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log("---------------");
+    setTemperature(Math.round(response.data.temperature.current));
+    console.log(temperature);
+  }
+  const apiKey = "4a9562b441206e7789dda73f03odae5t";
+  let city = "Lisbon";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="Weather">
       <form>
@@ -24,24 +36,32 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <h1>New York</h1>
+      <h1>{city}</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy </li>
       </ul>
 
       <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Weather"
-          />
-          <span className="temperature">6</span>
-          <span className="units">ºC</span>
+        <div className="col-6 ">
+          <div className="row">
+            <div className="col-6 ">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Weather"
+              />
+            </div>
+            <div className="col-4 ">
+              <span className="temperature ">{temperature}</span>
+            </div>
+            <div className="col-2 ">
+              <span className="units ">ºC</span>
+            </div>
+          </div>
         </div>
 
-        <div className="col-6">
-          <ul>
+        <div className="col-6 ">
+          <ul className="description">
             <li>Опади: 3%</li>
             <li>Вологість: 93% </li>
             <li>Вітер: 6 км/год</li>
